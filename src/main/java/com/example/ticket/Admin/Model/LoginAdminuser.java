6c268738527470ba44c171502376c0ca0aac3374
pulkitkaushik2003 <129057337+pulkitkaushik2003@users.noreply.github.com>
2025-09-02 22:00:22 +0530

    Update LoginAdminuser.java

--- src/main/java/com/example/ticket/Admin/Model/LoginAdminuser.java
+++ src/main/java/com/example/ticket/Admin/Model/LoginAdminuser.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class LoginAdminuser {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private  String role;
     private String username;
